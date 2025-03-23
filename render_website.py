ae02c6edfdfceec329af93913927abc6b87fec88
Trilok Nath <Trilok.Nath@ibm.com>
2025-03-23 09:41:04 +0530

    Initial commit

---
+++ render_website.py
@@ -0,0 +1,19 @@
+import json
+from jinja2 import Environment, FileSystemLoader
+
+# Load JSON data
+with open('website_json.json', 'r') as f:
+    data = json.load(f)
+
+# Configure Jinja2
+env = Environment(loader=FileSystemLoader('.'))  # '.' means current directory
+template = env.get_template('index_template.html') # main html file 
+
+# Render the template
+output_from_parsed_template = template.render(data=data)
+
+# Save the output to an HTML file
+with open("index.html", "w", encoding="utf-8") as fh:
+    fh.write(output_from_parsed_template)
+
+print("Template rendered successfully to index.html")
